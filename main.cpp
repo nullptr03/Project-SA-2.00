bf7e48f13392d55c1bfc8507e8afecb16251ba3d
4x00 <ayamgorengmanis87@gmail.com>
2022-04-15 01:22:05 +0800

    Add support for AML by RusJJ

--- main.cpp
+++ main.cpp
@@ -22,11 +22,36 @@ void *Init(void *p)
 	pthread_exit(0);
 }
 
+extern "C" void OnModLoad()
+{
+	__android_log_print(ANDROID_LOG_DEBUG, "AXLD", "Project SA library loaded from AML! Build time: " __DATE__ " " __TIME__);
+
+	g_libGTASA = ARMHook::getLibraryAddress("libGTASA.so");
+	if(g_libGTASA == 0)
+	{
+		__android_log_print(ANDROID_LOG_DEBUG, "AXLD", "ERROR: libGTASA.so address not found!");
+		return 0;
+	}
+
+	ARMHook::makeRET(g_libGTASA+0x3F6580);
+	ARMHook::initialiseTrampolines(g_libGTASA+0x3F6584, 0x2D2);
+
+	InstallGlobalHooks();
+	CProjectSA::InitPatch();
+	InitializeRenderWare();
+
+	pGame = new CGame();
+
+	InitScripting();
+	
+	Log::Initialize();
+}
+
 jint JNI_OnLoad(JavaVM *vm, void *reserved)
 {
 	__android_log_print(ANDROID_LOG_DEBUG, "AXLD", "Project SA library loaded! Build time: " __DATE__ " " __TIME__);
 
-	g_libGTASA = ARMHook::getLibraryAddress("_GTA3.so");
+	g_libGTASA = ARMHook::getLibraryAddress("lib_GTA3.so");
 	if(g_libGTASA == 0)
 	{
 		__android_log_print(ANDROID_LOG_DEBUG, "AXLD", "ERROR: libGTASA.so address not found!");
